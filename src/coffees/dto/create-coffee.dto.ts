b54b85c5c6680a6b08c6645925250a95fb23957b
ptanlam <phanlam20033002@gmail.com>
2021-08-06 09:34:29 +0700

    add swagger

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
+  @ApiProperty({ description: 'The name of coffee.' })
   @IsString()
   readonly name?: string;
 
+  @ApiProperty()
   @IsNumber()
   readonly price?: number;
 
+  @ApiProperty()
   @IsString({ each: true })
   readonly flavors?: string[];
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
@@ -17,11 +18,21 @@ async function bootstrap() {
       },
     })
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('ILuvCoffee 🤎')
+    .setDescription('Coffee application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(
     new WrapResponseInterceptor(),
     new TimeoutInterceptor()
   );
+
   await app.listen(3000);
 }
 bootstrap();
